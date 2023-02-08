d7ec5503068262e20b02759a0b1878fc2b9f18b7
Saransh Gupta <saranshg@tekion.com>
2023-02-08 18:23:13 +0530

    Made customised random function for batsman and bowler.

--- src/main/java/com/tekion/GameOfCricket/Services/PlayerService.java
+++ src/main/java/com/tekion/GameOfCricket/Services/PlayerService.java
@@ -4,6 +4,13 @@ import com.tekion.GameOfCricket.Models.Player;
 
 public class PlayerService {
     public static int getRuns(Player player){
-        return (int)(Math.random()*8);
+        if(player.getRole() == "Batsman"){
+            int[] runs = {0,0,1,1,1,1,2,2,2,2,3,3,4,4,6,6,7,7};
+            return runs[(int)(Math.random()*runs.length)];
+        }
+        else{
+            int[] runs = {0,0,1,1,1,2,2,3,4,6,7,7,7,7};
+            return runs[(int)(Math.random()*runs.length)];
+        }
     }
 }
