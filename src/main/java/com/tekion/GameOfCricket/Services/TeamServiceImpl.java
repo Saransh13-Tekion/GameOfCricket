663d615a05055a78e6b71bf6228ade5c47099407
Saransh Gupta <saranshg@tekion.com>
2023-02-18 23:48:53 +0530

    1. All Tables are now updated after every match. 2. Removed some unused imports.

--- src/main/java/com/tekion/GameOfCricket/Services/TeamServiceImpl.java
+++ src/main/java/com/tekion/GameOfCricket/Services/TeamServiceImpl.java
@@ -2,15 +2,7 @@ package com.tekion.GameOfCricket.Services;
 
 import com.tekion.GameOfCricket.Entity.MatchEntity;
 import com.tekion.GameOfCricket.Entity.TeamEntity;
-import com.tekion.GameOfCricket.Enums.PlayerRole;
-import com.tekion.GameOfCricket.Models.Team;
 import com.tekion.GameOfCricket.Repository.TeamRepository;
-import com.tekion.GameOfCricket.Utilities.Constants;
-import com.tekion.GameOfCricket.Models.Player;
-import com.github.javafaker.Faker;
-import jakarta.persistence.GeneratedValue;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
